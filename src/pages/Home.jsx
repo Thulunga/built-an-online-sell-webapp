a9e21b788e7889dc311a7792c13fba89303e00e0
Thulunga <thulunga.tb@gmail.com>
2022-01-24 13:03:10 +0530

    Slider component and fake data is added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,19 @@ import Announcement from "../components/Announcement";
 //import Footer from "../components/Footer";
 //import Newsletter from "../components/Newsletter";
 //import Products from "../components/Products";
-//import Slider from "../components/Slider";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
+      
 
       <Announcement />
-
-      {/* 
       <Navbar />
       <Slider />
+      
+      {/* 
+      
       <Categories />
       <Products/>
       <Newsletter/>
